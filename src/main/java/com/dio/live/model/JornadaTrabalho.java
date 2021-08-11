102f372346b3280fa93332f3b8b7394669432333
Fabiano Scorsatto <fabianoscorsatto@gmail.com>
2021-08-11 16:27:01 -0400

    Commit - Aula 2
    Código da aula 2.

---
+++ src/main/java/com/dio/live/model/JornadaTrabalho.java
@@ -0,0 +1,16 @@
+package com.dio.live.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class JornadaTrabalho {
+
+    private long id;
+    private String descricao;
+
+}
